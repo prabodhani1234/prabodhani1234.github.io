c07052c94617a5cdfad1d9e6cb6ae20e898760b2
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2022-02-27 15:07:25 +0530

    Updates

---
+++ precache-manifest.79304fc1cff1937f454d1438ea897688.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3437dd6855a9cf0f377daf492b45f27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "848a8c3721c9933eec73",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "5022c52bde2c9cf11d1e",
+    "url": "/static/css/main.5ea5d382.chunk.css"
+  },
+  {
+    "revision": "848a8c3721c9933eec73",
+    "url": "/static/js/2.0897c9b1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0897c9b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5022c52bde2c9cf11d1e",
+    "url": "/static/js/main.6c5a1571.chunk.js"
+  },
+  {
+    "revision": "abae84448ea5f649d076",
+    "url": "/static/js/runtime-main.6f12e435.js"
+  },
+  {
+    "revision": "390fc91bab1a99e1b10c88d5f2444487",
+    "url": "/static/media/image1.390fc91b.png"
+  },
+  {
+    "revision": "4a72cc97fded3accab9c9a0b2f02b21f",
+    "url": "/static/media/image2.4a72cc97.png"
+  },
+  {
+    "revision": "6009cc8016b5ba09439c904ddf2df6e8",
+    "url": "/static/media/image3.6009cc80.png"
+  },
+  {
+    "revision": "927875b8f6ad89df2ec134a3dbc9159d",
+    "url": "/static/media/laptop.927875b8.png"
+  },
+  {
+    "revision": "75b4a4ff81400e9eab1e58488fc42469",
+    "url": "/static/media/profile.75b4a4ff.png"
+  }
+]);
